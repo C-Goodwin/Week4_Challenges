e0d87b778050f444874e9306f59be03fc322172a
C-Goodwin <73571245+C-Goodwin@users.noreply.github.com>
2020-12-05 16:16:52 -0500

    Add project files.

---
+++ Week4_Challenges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Challenges
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        
+        {
+
+           public PetInfo snowWhite = new PetInfo("Ernest Hemingway", "907 Whitehead St, Key West, FL 33040"
+           ,"3052942755", "IsDead@gmail.com", 48, "Snow White", "Cat", "Polydactyl", "White", false, true);
+        public BillingInfo snowWhiteBilling = new BillingInfo("Ernest Hemingway",
+            "907 Whitehead St, Key West, FL 33040", "3052942755", "IsDead@gmail.com", 48,
+            2, 298146892607369, "08/24", 804);
+            
+        }
+    }
+}
---
+++ Week4_Challenges/Week4_Challenges.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Challenges
+{
+    abstract class OwnerInfo
+    {
+        public string OwnerName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int NumberOfPets {get; set;}
+
+        public OwnerInfo() { }
+
+        public OwnerInfo(string ownerName, string address, string phoneNumber,
+          string email, int numOfPets)
+        {
+            OwnerName = ownerName;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            NumberOfPets = numOfPets;
+        }
+    }
+    
+    class PetInfo : OwnerInfo
+    {
+        public string PetName { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public bool IsChipped { get; set; }
+        public bool IsFixed { get; set; }
+
+        public PetInfo() { }
+        public PetInfo(string ownerName, string address, string phoneNumber,
+          string email, int numOfPets, string petName, string species, string breed, string color,
+            bool isChipped, bool isFixed)
+            :base(ownerName,address,phoneNumber, email, numOfPets)
+        {
+            PetName = petName;
+            Species = species;
+            Breed = breed;
+            Color = color;
+            IsChipped = isChipped;
+            IsFixed = isFixed;
+        }
+    }
+    public enum MethodOfPayment
+    {
+            Cash = 1,
+            Check,
+            CreditCard,
+            DebitCard
+
+    }
+    class BillingInfo : OwnerInfo
+    {
+        public MethodOfPayment MethodOfPayment { get; set; }
+        public long CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public int CVV { get; set; }
+
+        public BillingInfo() { }
+        public BillingInfo(string ownerName, string address, string phoneNumber,
+          string email, int numOfPets, MethodOfPayment method, long cardNum, string expDate, int cvv)
+            :base(ownerName, address, phoneNumber, email, numOfPets)
+        {
+            MethodOfPayment = method;
+            CardNumber = cardNum;
+            ExpirationDate = expDate;
+            CVV = cvv;
+        }
+    }
+}
